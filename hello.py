25fb3c3f909ba5356e0880dfe77fedf06f897da3
Drapple5 <3277939480@qq.com>
2019-07-15 21:10:27 +0800

    Ass index page

--- hello.py
+++ hello.py
@@ -1,4 +1,29 @@
-from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for,render_template
+movies = [
+{'title':'Dead Poets Society','year':'1989'},
+{'title':'My Neighbor Totoro','year':'1988'},
+{'title':'A Perfect World','year':'1933'},
+{'title':'Leon','year':'1994'},
+{'title':'Mahjong','year':'1996'},
+{'title':'Swallowtail Butterfly','year':'1996'},
+{'title':'King of Comedy','year':'1999'},
+{'title':'Devils on the Doorstep','year':'1999'},
+{'title':'WALL-E','year':'2008'},
+{'title':'The Pork of Music','year':'2012'},
+]
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
+
 app = Flask(__name__)
 @app.route('/')
 def Hello( ):
@@ -22,4 +47,13 @@ def weizhuce():
 @app.route('/请注册11')
 def qingzhuce():
     return'<h1>请输入必要信息</h1>'
-#http://localhost:5000/%E8%AF%B7%E6%B3%A8%E5%86%8C11111
+
+@app.route('/user/<int:age>')
+def user(age):
+    if age<18:
+        return "不好意思这儿是个18禁网站"
+    if age>=18:
+        return "欢迎呀"
+@app.route('/<name>')
+def index(name):
+    return render_template('index.html',name=name,movies=movies)
